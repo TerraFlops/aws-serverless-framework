d2efa0803ccd0c735869c7f0eb68f6cf4540cc25
Damian Sloane <damian@capacitorlabs.com>
2020-07-31 17:02:34 +1000

    Work in progress

--- output.tf
+++ output.tf
@@ -1,24 +1,24 @@
 output "iam_role" {
   description = "IAM role resource created for deployment of Serverless framework projects"
-  value = var.create_iam_role == true ? aws_iam_role.serverless_deployment_role : null
+  value = var.create_iam_role == true ? aws_iam_role.serverless_deployment_role[0] : null
 }
 
 output "iam_role_arn" {
   description = "IAM role ARN created for deployment of Serverless framework projects"
-  value = var.create_iam_role == true ? aws_iam_role.serverless_deployment_role.arn : null
+  value = var.create_iam_role == true ? aws_iam_role.serverless_deployment_role[0].arn : null
 }
 
 output "artifact_bucket" {
   description = "S3 bucket resource created for deployment of Serverless framework artifacts"
-  value = var.create_deployment_bucket == true ? aws_s3_bucket.serverless_deployment_bucket : null
+  value = var.create_deployment_bucket == true ? aws_s3_bucket.serverless_deployment_bucket[0] : null
 }
 
 output "artifact_bucket_arn" {
   description = "S3 bucket ARN created for deployment of Serverless framework artifacts"
-  value = var.create_deployment_bucket == true ? aws_s3_bucket.serverless_deployment_bucket.arn : null
+  value = var.create_deployment_bucket == true ? aws_s3_bucket.serverless_deployment_bucket[0].arn : null
 }
 
 output "artifact_bucket_name" {
   description = "S3 bucket name created for deployment of Serverless framework artifacts"
-  value = var.create_deployment_bucket == true ? aws_s3_bucket.serverless_deployment_bucket.bucket : null
+  value = var.create_deployment_bucket == true ? aws_s3_bucket.serverless_deployment_bucket[0].bucket : null
 }
